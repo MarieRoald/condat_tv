0a40b1081a986cfb70b6a7c4c3269e1e81f9eaca
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2020-11-20 08:55:39 +0100

    Added more input sanitising

--- src/condat_tv/tv.pyx
+++ src/condat_tv/tv.pyx
@@ -26,8 +26,13 @@ cpdef tv_denoise(signal, regularisation_strength, out=None):
     regularisation_strength: float
     out: np.ndarray[double, ndim=1] (same size as signal, optional)
     """
+    signal = np.ascontiguousarray(signal, dtype=np.double)
     if out is None:
         out = np.empty_like(signal)
+    elif not out.data.c_contiguous:
+        raise ValueError("Output vector must be C-contiguous")
+    elif out.dtype != np.double:
+        raise ValueError("Output vector must have dtype=np.double")
 
     c_tv_denoise(signal, out, len(signal), regularisation_strength)
     return out
@@ -49,12 +54,14 @@ cpdef tv_denoise_matrix(signal, regularisation_strength, out=None):
     # signal[i, :] is anoter contiguous array
     # signal[:, 0] is not a contigous array
     # signal[:, i] is also not a contiguous array 
-    signal = np.ascontiguousarray(signal)
+    signal = np.ascontiguousarray(signal, dtype=np.double)
 
     if out is None:
         out = np.empty_like(signal)
     elif not out.data.c_contiguous:
         raise ValueError("Output matrix must be C-contiguous")
+    elif out.dtype != np.double:
+        raise ValueError("Output matrix must have dtype=np.double")
     
     num_rows, row_length = signal.shape
     for i in range(num_rows):
